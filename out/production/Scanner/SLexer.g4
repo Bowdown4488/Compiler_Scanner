0eda08442f9733940c6d73df4563f424c1d4e8c3
KillerTrex1 <37259376+KillerTrex1@users.noreply.github.com>
2019-10-14 04:46:36 +0800

    S C A N N E R H A R D C O D E

--- out/production/Scanner/SLexer.g4
+++ out/production/Scanner/SLexer.g4
@@ -1,6 +1,58 @@
+//lexer grammar SLexer;
 lexer grammar SLexer;
 
-STRING : ('a'..'z'|'A'..'Z'|'0'..'9'|'_'|'@')+ |
+/*
+ABSTRACT : 'abstrakto';
+BOOLEAN : 'boorean';
+BREAK : 'bureko';
+BYTE : 'byto';
+CASE : 'kesu';
+CATCH : 'katchu';
+CHAR : 'karu';
+CLASS : 'kurasu';
+CONST : 'konst';
+CONTINUE : 'kontinu';
+DEFAULT : 'deporuto';
+DO : 'do';
+DOUBLE : 'doburu';
+ELSE : 'erusu';
+ENUM : 'enumu';
+EXTENDS : 'extendo';
+FINAL : 'finar';
+FINALLY : 'finarri';
+FLOAT : 'furoto';
+FOR : 'foru';
+IF : 'ifu';
+GOTO : 'iku';
+IMPLEMENTS : 'impremento';
+IMPORT : 'importo';
+INSTANCEOF : 'insutaof';
+INT : 'into';
+INTERFACE : 'intefeseru';
+LONG : 'rongu';
+NATIVE : 'netivu';
+NEW : 'newu';
+PACKAGE : 'pakeju';
+PRIVATE : 'privatto';
+PROTECTED : 'protekto';
+PUBLIC : 'pubriko';
+RETURN : 'returno';
+SHORT : 'shorto';
+STATIC : 'statiku';
+STRICTFP : 'strictofp';
+SUPER : 'supa';
+SWITCH : 'switchu';
+SYNCHRONIZED : 'sinkuronaizu';
+THIS : 'disu';
+THROW : 'throwu';
+THROWS : 'throwsu';
+TRANSIENT : 'transento';
+TRY : 'tryu';
+VOID : 'boido';
+VOLATILE : 'voratiru';
+WHILE : 'wairu';
+*/
+STRING : ('a'..'z'|'A'..'Z'|'0'..'9'|'_'|'@'|'.')+ |
          ('"' (~('"' | '\\' | '\r' | '\n') | '\\' ('"' | '\\'))* '"') ;
 
 // Skip all spaces, tabs, newlines
--- src/TestLexer.java
+++ src/TestLexer.java
@@ -1,6 +1,8 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
 
+import java.util.ArrayList;
+
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
 public class TestLexer {
@@ -14,10 +16,37 @@ public class TestLexer {
         SLexer lexer = new SLexer(input);
 
         Token token = lexer.nextToken();
+        ArrayList<Token> tokens = new ArrayList<>();
+        ArrayList<String> tokentypes = new ArrayList<>();
         while (token.getType() != SLexer.EOF) {
-            System.out.println("\t" + token.getText() + "\t\t\t\t"  + getTokenType(token.getType(),token.getText()));
+            //System.out.println("\t" + token.getText() + "\t\t\t\t"  + getTokenType(token.getType(),token.getText()));
+            tokens.add(token);
+            tokentypes.add(getTokenType(token.getType(),token.getText()));
             token = lexer.nextToken();
         }
+        System.out.println(tokens.size()+" "+tokentypes.size());
+        for(int i = 0;i<tokentypes.size();i++){
+
+            if(tokentypes.get(i).equals("String")){
+                boolean identifier = false;
+                if(i-1>=0){
+                    if(tokentypes.get(i-1).equals("Keyword")){
+                        identifier = true;
+                    }
+                }else if(i+1!=tokens.size()){
+                    if(tokentypes.get(i+1).equals("String"))
+                        identifier = true;
+                }
+
+                if(identifier)
+                    tokentypes.set(i,"Identifier");
+                else
+                    tokentypes.set(i,"Literal");
+            }
+            System.out.println("\t" + tokens.get(i).getText() + "\t\t\t\t"  +tokentypes.get(i));
+        }
+
+
     }
     static String[] keywords = {"abstrakto","boorean","bureko","byto","kesu","katchu","karu","kurasu","konst","kontinu","deporuto","do","doburu","erusu","enumu","extendo","finar","finarri","furoto","foru","ifu","iku","impremento","importo","insutaof","into","intefeseru","rongu","netivu","newu","pakeju","privatto","protekto","pubriko","returno","shorto","statiku","strictofp","supa","switchu","sinkuronaizu","disu","throwu","throwsu","transento","tryu","boido","voratiru","wairu"};
     private static String getTokenType(int tokenType,String tokenString) {
@@ -30,87 +59,47 @@ public class TestLexer {
                     return "String";
                 }
             case SLexer.LPAREN:
-                return "Separators";
             case SLexer.RPAREN:
-                return "Separators";
             case SLexer.LBRACK:
-                return "Separators";
             case SLexer.RBRACK:
-                return "Separators";
-            case SLexer.EQUALS:
-                return "Operator";
             case SLexer.SEMICO:
-                return "Separators";
+                return "Separator";
+            case SLexer.EQUALS:
             case SLexer.ASSIGN:
-                return "Operator";
             case SLexer.LESSTN:
-                return "Operator";
             case SLexer.GESSTN:
-                return "Operator";
             case SLexer.NOTOPN:
-                return "Operator";
             case SLexer.APPROX:
-                return "Operator";
             case SLexer.OPTION:
-                return "Operator";
             case SLexer.COLON:
-                return "Operator";
             case SLexer.DEQUAL:
-                return "Operator";
             case SLexer.LEQUAL:
-                return "Operator";
             case SLexer.GEQUAL:
-                return "Operator";
             case SLexer.NEQUAL:
-                return "Operator";
             case SLexer.ANDD:
-                return "Operator";
             case SLexer.ORR:
-                return "Operator";
             case SLexer.INCR:
-                return "Operator";
             case SLexer.DECR:
-                return "Operator";
             case SLexer.ADD:
-                return "Operator";
             case SLexer.MINUS:
-                return "Operator";
             case SLexer.TIMES:
-                return "Operator";
             case SLexer.DIVIDE:
-                return "Operator";
             case SLexer.AND:
-                return "Operator";
             case SLexer.OR:
-                return "Operator";
             case SLexer.EXP:
-                return "Operator";
             case SLexer.PERC:
-                return "Operator";
             case SLexer.DLEFT:
-                return "Operator";
             case SLexer.RIGHT:
-                return "Operator";
             case SLexer.QRIGHT:
-                return "Operator";
             case SLexer.PEQUAL:
-                return "Operator";
             case SLexer.MEQUAL:
-                return "Operator";
             case SLexer.TEQUAL:
-                return "Operator";
             case SLexer.SEQUAL:
-                return "Operator";
             case SLexer.AEQUAL:
-                return "Operator";
             case SLexer.OEQUAL:
-                return "Operator";
             case SLexer.EEQUAL:
-                return "Operator";
             case SLexer.REQUAL:
-                return "Operator";
             case SLexer.DLEQUAL:
-                return "Operator";
             case SLexer.DRQUAL:
                 return "Operator";
             default:
@@ -119,7 +108,7 @@ public class TestLexer {
     }
     private static boolean findKeyword(String s){
         for(int i=0;i<keywords.length;i++){
-            if(s.equals(keywords[i]))
+            if(s.equalsIgnoreCase(keywords[i]))
                 return true;
         }
         return false;
